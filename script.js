50e609a7be1ae545a9d643fc70872e52fab03ef3
LuisIriarte5 <luisalejandroiriarte@gmail.com>
2023-12-09 16:22:26 +0100

    Create functions for add, substract, multiply, and divide

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Works');
+
+let a = add(1,2,3,4);
+let s = substract(5,4);
+let m = multiply(3,4,5);
+let d = divide(10,5);
+
+function add (...array) {
+    return array.reduce((total, item) => total+item,0);
+}
+
+function substract (...array) {
+    return array.reduce((total, item) => total-item);
+}
+
+function multiply (...array) {
+    return array.reduce((total, item) => total*item,1);
+}
+
+function divide (...array) {
+    return array.reduce((total, item) => total/item);
+}
