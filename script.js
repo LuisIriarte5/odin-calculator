08b9deefbdb405de6837cf80cb09c22c4db20910
LuisIriarte5 <luisalejandroiriarte@gmail.com>
2023-12-09 16:58:42 +0100

    Add a calculator in the UI
    A visual representation of a calculator in
    the screen was created including event listener
    for all the buttons and show their value in the
    console when pressed.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-
 let a = add(1,2,3,4);
 let s = substract(5,4);
 let m = multiply(3,4,5);
 let d = divide(10,5);
+
+let op = operate(5,4,'+');
 
 function add (...array) {
     return array.reduce((total, item) => total+item,0);
@@ -19,3 +20,31 @@ function multiply (...array) {
 function divide (...array) {
     return array.reduce((total, item) => total/item);
 }
+
+function operate (num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return substract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+    }
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+let pressedButton = document.querySelectorAll('button');
+pressedButton.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        console.log(event.srcElement.textContent);
+    });
+});
