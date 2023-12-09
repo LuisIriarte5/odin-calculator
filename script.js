eaebcecb110509a1d5e2a55f99ceae66b8f4dbfd
LuisIriarte5 <luisalejandroiriarte@gmail.com>
2023-12-09 17:43:55 +0100

    Conclude a functional calculator but only for one operation

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-let a = add(1,2,3,4);
-let s = substract(5,4);
-let m = multiply(3,4,5);
-let d = divide(10,5);
-
-let op = operate(5,4,'+');
-
 function add (...array) {
     return array.reduce((total, item) => total+item,0);
 }
@@ -19,6 +12,15 @@ function multiply (...array) {
 
 function divide (...array) {
     return array.reduce((total, item) => total/item);
+}
+
+function populateDisplay (value) {
+    if (!isSecond) {
+        firstNumber.push(value);
+    } else {
+        secondNumber.push(value);
+    }
+    screenContent.textContent += value;
 }
 
 function operate (num1, num2, operator) {
@@ -38,13 +40,38 @@ function operate (num1, num2, operator) {
     }
 }
 
-let firstNumber;
-let secondNumber;
+let firstNumber = [];
+let secondNumber = [];
 let operator;
+let isSecond = false;
 
 let pressedButton = document.querySelectorAll('button');
 pressedButton.forEach((item) => {
     item.addEventListener('click', (event) => {
-        console.log(event.srcElement.textContent);
+        switch (event.srcElement.className) {
+            case 'number':
+                console.log('number');
+                populateDisplay(event.srcElement.textContent);
+                break;
+            case 'operator':
+                operator = event.srcElement.textContent;
+                isSecond = true;
+                screenContent.textContent = '';
+                console.log('operator');
+                break;
+            case 'equal':
+                let res = operate(Number(firstNumber.join('')),Number(secondNumber.join('')),operator);
+                screenContent.textContent = res;
+                console.log('equal');
+                break;
+            case 'clear':
+                console.log('clear button');
+                break;
+        }
+        // screenContent.textContent = event.srcElement.textContent;
     });
 });
+
+const screen = document.querySelector('#display');
+const screenContent = document.createElement('p');
+screen.appendChild(screenContent);
